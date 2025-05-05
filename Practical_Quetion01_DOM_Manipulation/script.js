793844cd9f6c6deb458784cfeb542bb467a663b2
Waqas-Khan-CodeCanvas <waqaskhan11411704@gmail.com>
2025-05-06 02:04:21 +0500

    question 01 in mid exam

---
+++ Practical_Quetion01_DOM_Manipulation/script.js
@@ -0,0 +1,9 @@
+
+const btn = document.getElementById('btn')
+const text = document.getElementById('text')
+
+function btnClick() {
+    btn.style.backgroundColor="blue";
+    text.textContent="Button Clicked!"
+}
+
